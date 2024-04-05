048638adeb6fd195481268301c1a315bf5b14c07
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-04-05 15:18:51 -0300

    feat: create car details screen

---
+++ src/screens/car-details/car-details.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export function CarDetailsScreen() {
+    return (
+        <View>
+            <Text>CarDetailsScreen</Text>
+        </View>
+    );
+}
---
+++ src/screens/car-details/index.ts
@@ -0,0 +1,1 @@
+export { CarDetailsScreen as default } from "./car-details";
